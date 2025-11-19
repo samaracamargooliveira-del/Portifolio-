f0f61ce5d585c9462d92bcc83d9bab287f445ae3
samara camargo <samara.camargo.oliveira@escola.pr.gov.br>
2025-11-19 14:39:53 -0300

    19/11/2025

---
+++ script.js
@@ -0,0 +1,12 @@
+ //let texto = document.querySelector ("#nome");
+ //texto.textContent = "Samara";
+ 
+ let nome = prompt ("Qual é o seu nome?")
+
+   TextDecoder.texContent = nome;
+
+   if (nome ==){ 
+       TextDecoder.texContent = ' seja Bem vindo;
+   } else{
+        TextDecoder.textContent = nome;
+   }
